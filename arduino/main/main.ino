e2329aebfd5074695fa67837266139fbebd5afa6
GFMws <ydalite01@gmail.com>
2025-06-18 13:59:48 +0400

    analog read

---
+++ arduino/main/main.ino
@@ -0,0 +1,27 @@
+
+
+#define key_pin 1
+
+
+
+int key_value = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+
+
+
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  key_value = analogRead(key_pin);
+
+  // Отправляем значение в Serial Monitor
+  Serial.print("Аналоговое значение: ");
+  Serial.println(key_value);
+}
