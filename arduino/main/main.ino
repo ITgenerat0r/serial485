089fde17252dd6e066fbcf6bd077cce7b1297d18
lattepanda <lattepanda@gfm.com>
2025-06-19 10:35:28 +0400

    select by resistance

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,10 +1,14 @@
 
+#include <String.h>
 
-#define key_pin 1
+#define key_pin 6
 
 
 
 int key_value = 0;
+float v = 0;
+
+String tp = "";
 
 void setup() {
   // put your setup code here, to run once:
@@ -21,9 +25,41 @@ void loop() {
 
   key_value = analogRead(key_pin);
 
+  v = 1.0 * map(key_value, 0, 1024, 0, 500) / 100;
+
   // Отправляем значение в Serial Monitor
   Serial.print("Аналоговое значение: ");
-  Serial.println(key_value);
+  Serial.print(key_value);
+  Serial.print(",  (");
+  Serial.print(v);
+  Serial.println("v).");
+  if (v < 0.75){
+  	if (v < 0.25){
+  		Serial.println("PAS. R=None");
+	} else {
+		Serial.println("PAS. R=91kOm");
+	}
+	tp = "PAS";
+  } else if (v > 0.75 && v < 1.25){
+  	Serial.println("Free. R=39kOm");
+  } else if (v > 1.25 && v < 1.75){
+  	Serial.println("Free. R=24kOm");
+  } else if (v > 1.75 && v < 2.25){
+  	Serial.println("Free. R=15kOm");
+  } else if (v > 2.25 && v < 2.75){
+  	Serial.println("Free. R=10kOm");
+  } else if (v > 2.75 && v < 3.25){
+  	Serial.println("Free. R=6.8kOm");
+  } else if (v > 3.25 && v < 3.75){
+  	Serial.println("Free. R=4.3kOm");
+  } else if (v > 3.75 && v < 4.25){
+  	Serial.println("DOL. R=2.4kOm");
+  	tp = "DOL";
+  } else if (v > 4.25 && v < 4.75){
+  	Serial.println("Free. R=1kOm");
+  } else if (v > 4.75){
+  	Serial.println("Free. R=?Om");
+  }
 
   delay(1000);
 }
